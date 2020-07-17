cf50fd65f4fe433c8f78bd829ac2664dc957acd3
Kam Ndirangu <kam.ndirangu@perficient.com>
2020-07-17 16:44:49 -0400

    completed base structure

---
+++ Boot_Track/Models/Progress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Boot_Track.Models
+{
+    public class Progress
+    {
+      
+        public Intern intern { get; set; }
+
+        public Module module { get; set; }
+
+        public int progress { get; set; }
+    }
+}
