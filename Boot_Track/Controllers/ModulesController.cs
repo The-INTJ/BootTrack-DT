3ee6f16c376ff46b2a2a54d9311b1b4eace8d677
Alex Karadsheh <alex.karadsheh@perficient.com>
2020-07-18 15:39:35 -0400

    Index dynamically renders correctly. Module page does not - receiving a null model exception from data passing errors. Not passing data between views correctly.,

--- Boot_Track/Controllers/ModulesController.cs
+++ Boot_Track/Controllers/ModulesController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Boot_Track.Models;
 
 namespace Boot_Track.Controllers
 {
     public class ModulesController : Controller
     {
         // GET: Modules
-        public ActionResult ModulePage()
+        public ActionResult ModulePage(HttpContext context)
         {
-            return View();
+            var sesh = new Session();
+            sesh.GetModules();
+            foreach (var module in sesh.modules)
+            {
+                if (module.Title != ModuleTitle)
+                {
+                    module.Title = (module.Title == ModuleTitle);
+                    return View(module);
+                }
+            }
+            return View(sesh.modules[0]);
         }
     }
 }
