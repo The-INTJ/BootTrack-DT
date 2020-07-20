235dce98a080bdb847d39e852a086deb82575688
Drew Taylor <drew.taylor@perficient.com>
2020-07-20 10:43:12 -0400

    Created login page, controller, and css styling

---
+++ Boot_Track/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Boot_Track.Models
+{
+    public class Login
+    {
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsLoggedIn { get; set; }
+
+
+    }
+}
